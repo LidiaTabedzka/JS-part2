a23f246c3d4cd41dfb7b367ace9a6c3db1c76f10
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2017-11-06 11:37:06 +0100

    Task 3 first word will be replaced

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 var dinosaur = "triceratops";
 
-var newText = text.replace("Velociraptor", dinosaur.toUpperCase());
-var finalText = newText.substr(0, newText.length/2);
+var firstWord = text.split(' ', 1);
+
+var newText = text.replace(firstWord, dinosaur.toUpperCase());
+var finalText = newText.substr(0, newText.length / 2);
 
 console.log(newText);
 console.log(finalText);
